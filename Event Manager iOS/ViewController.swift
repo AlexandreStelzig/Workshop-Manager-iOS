3e5e39659e9fd362c43880e6369a767044825e51
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2018-01-16 19:38:29 -0500

    first commit

---
+++ Event Manager iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Event Manager iOS
+//
+//  Created by Alexandre Stelzig on 2018-01-16.
+//  Copyright © 2018 Event Manager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Event Manager iOSTests/Event_Manager_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Event_Manager_iOSTests.swift
+//  Event Manager iOSTests
+//
+//  Created by Alexandre Stelzig on 2018-01-16.
+//  Copyright © 2018 Event Manager. All rights reserved.
+//
+
+import XCTest
+@testable import Event_Manager_iOS
+
+class Event_Manager_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
